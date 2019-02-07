0b3d7013f139ffa79851394213432f279d3168e1
ienvenue <chenyoungjie@163.com>
2019-02-07 21:59:59 +0800

    [migrateMessages]

--- app/User.php
+++ app/User.php
@@ -71,6 +71,11 @@ class User extends Authenticatable
         return !!$this->votes()->where('answer_id', $answer)->count();
     }
 
+    public function messages()
+    {
+        return $this->hasMany(Message::class,'to_user_id');
+    }
+
     /**
      * @param $question
      * @return array
